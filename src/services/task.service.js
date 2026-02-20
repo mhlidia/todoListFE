51cbcdf6de2977fb97046398fee7780efbba70d7
mhlidia <224844611+mhlidia@users.noreply.github.com>
2026-02-20 18:46:48 -0400

    BOOT-5: development of the getall() function

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -3,7 +3,25 @@ const API_URL = "http://127.0.0.1:8000/api/tasks";
 export const tareaService = {
 
     getAll: async () => {
-        // se implementará en el Ticket 5
+        try {
+            const response = await fetch(API_URL, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error("Error fetching tasks");
+            }
+
+            return await response.json();
+
+        } catch (error) {
+            console.error("Error in getAll:", error);
+            throw error;
+        }
     },
 
     create: async (data) => {
