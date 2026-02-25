68203e9a55d9dac47e48028bf820008d3dae4ea8
mhlidia <224844611+mhlidia@users.noreply.github.com>
2026-02-25 12:11:23 -0400

    BOOT-6: Update import after service refactor

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useEffect } from "react";
-import { taskService } from "./services/task.service";
+import { getAllTasks } from "./services/task.service";
 
 function App() {
 
   useEffect(() => {
 
-    const fetchTasks = async () => {
-      try {
-        const data = await taskService.getAll();
-        console.log("Tasks from API:", data);
-      } catch (error) {
-        console.error("Error fetching tasks:", error);
-      }
-    };
+  const fetchTasks = async () => {
+    try {
+      const data = await getAllTasks();
+      console.log("Tasks from API:", data);
+    } catch (error) {
+      console.error("Error fetching tasks:",error);
+    }
+  };
 
-    fetchTasks();
+  fetchTasks();
 
-  }, []);
+}, []);
 
   return (
     <>
