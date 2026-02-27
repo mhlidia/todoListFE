8358b9d323a26ada6ddbf44162f4b79fd10ad8c9
mhlidia <224844611+mhlidia@users.noreply.github.com>
2026-02-27 10:15:40 -0400

    BOOT-7: The comment and try-catch service were removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,3 @@
-/*import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useEffect } from "react";
-import { getAllTasks } from "./services/task.service";
-
-function App() {
-
-  useEffect(() => {
-
-  const fetchTasks = async () => {
-    try {
-      const data = await getAllTasks();
-      console.log("Tasks from API:", data);
-    } catch (error) {
-      console.error("Error fetching tasks:",error);
-    }
-  };
-
-  fetchTasks();
-
-}, []);
-
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  );
-}
-
-export default App;*/
-
 import { useEffect, useState } from "react";
 import { getAllCategories } from "./services/category.service";
 
