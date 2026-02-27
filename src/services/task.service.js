f78fe558067f9b1240c0e981e0f9cd53dbc41322
Lidia J. Mayta Huanca <224844611+mhlidia@users.noreply.github.com>
2026-02-27 18:49:30 -0400

    BOOT-13: Add task creation with category and tags (#12)

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -3,17 +3,21 @@ import { API_BASE_URL } from "./index";
 const TASK_URL = `${API_BASE_URL}tasks`;
 
 export const getAllTasks = async () => {
-    try {
-        const response = await fetch(TASK_URL);
-
-        if (!response.ok) {
-            throw new Error("Error fetching tasks");
-        }
-
-        return await response.json();
-    } catch (error) {
-        console.error("Error in getAllTasks:", error);
-        throw error;
-    }
+  const response = await fetch(TASK_URL);
+  if (!response.ok) throw new Error("Error fetching tasks");
+  return await response.json();
 };
 
+export const createTask = async (data) => {
+  const response = await fetch(TASK_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  if (!response.ok) throw new Error("Error creating task");
+  return await response.json();
+};
